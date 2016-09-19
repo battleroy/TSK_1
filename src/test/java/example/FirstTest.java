1a4f281b070ad16613244277052c6815be544966
battleroy <cnam.sux@gmail.com>
2016-09-19 20:43:03 +0300

    - Golf 2 Test - Correct Login Test - Incorrect Login Test - Show All Brands Test - Logout Test

--- src/test/java/example/FirstTest.java
+++ src/test/java/example/FirstTest.java
@@ -2,6 +2,7 @@ package example;
 
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.SelenideElement;
 import org.openqa.selenium.By;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
@@ -10,8 +11,6 @@ import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
 public class FirstTest {
-
-    //TODO: 5 TESTOV SDELAT S PROVERKAMI
 
     private ChromeDriver chromeDriver = new ChromeDriver();
     private String url = "http://av.by/";
@@ -31,23 +30,99 @@ public class FirstTest {
         chromeDriver.findElementByXPath(XPath).click();
     }
 
-    @Test
-    public void userCanLoginByUsername() {
+    private void startup() {
         Configuration.browser = "chrome";
         open(url);
-        $(By.xpath(".//*[@id='submit_presearch']")).click();
-        $(By.xpath(".//*[@id='brand_id_chosen']/a/span")).shouldBe(Condition.visible);
-        $(By.xpath(".//*[@id='brand_id_chosen']/a/span")).click();
-        $(By.xpath(".//*[@id='brand_id_chosen']/div/ul/li[80]")).click();
-        $(By.xpath(".//*[@id='model_id_chosen']/a/span")).click();
-        $(By.xpath(".//*[@id='model_id_chosen']/div/ul/li[13]")).click();
-        $(By.xpath(".//*[@id='region_id_filter_chosen']/a/span")).click();
-        $(By.xpath(".//*[@id='region_id_filter_chosen']/div/ul/li[6]")).click();
-        $(By.xpath(".//*[@id='search_extend_link']")).click();
-        $(By.xpath(".//*[@id='PublicPassengerFilter']/div/div[3]/div[1]/div/div[2]/div[1]/input")).setValue("1800");
-        $(".//*[@id='PublicPassengerFilter']/div/div[9]/input[1]").click();
     }
 
+    @Test
+    public void golf() {
+        startup();
+        SelenideElement searchButton = $(By.xpath(".//*[@id='submit_presearch']"));
+        SelenideElement brandDropdown = $(By.xpath(".//*[@id='brand_id_chosen']/a/span"));
+        SelenideElement vwSelection = $(By.xpath(".//*[@id='brand_id_chosen']/div/ul/li[80]"));
+        SelenideElement modelDropdown = $(By.xpath(".//*[@id='model_id_chosen']/a/span"));
+        SelenideElement golfSelection = $(By.xpath(".//*[@id='model_id_chosen']/div/ul/li[13]"));
+        SelenideElement regionFilter = $(By.xpath(".//*[@id='region_id_filter_chosen']/a/span"));
+        SelenideElement minskRegionSelection = $(By.xpath(".//*[@id='region_id_filter_chosen']/div/ul/li[6]"));
+        SelenideElement extendedSearchButton = $(By.xpath(".//*[@id='search_extend_link']"));
+        SelenideElement inputMinimumDisplacement = $(By.xpath(".//*[@id='PublicPassengerFilter']/div/div[3]/div[1]/div/div[2]/div[1]/input"));
+        SelenideElement startSearchButton = $(".//*[@id='PublicPassengerFilter']/div/div[9]/input[1]");
 
+        searchButton.click();
+        brandDropdown.shouldBe(Condition.visible).click();
+        vwSelection.click();
+        modelDropdown.waitUntil(Condition.visible, 20).click();
+        golfSelection.click();
+        regionFilter.click();
+        minskRegionSelection.click();
+        extendedSearchButton.click();
+        inputMinimumDisplacement.shouldBe(Condition.visible).setValue("1800");
+        startSearchButton.click();
+
+        brandDropdown.shouldBe(Condition.exactText("Volkswagen"));
+        modelDropdown.shouldBe(Condition.exactText("Golf 2"));
+    }
+
+    @Test
+    public void showAllBrands() {
+        startup();
+        SelenideElement showAllButton = $(By.xpath("html/body/div[3]/div[1]/main/div[4]/div[1]/div/p/a"));
+        SelenideElement exclusiveLink = $(By.xpath("html/body/div[3]/div[1]/main/div[4]/div[1]/div/ul/li[95]/a/span"));
+
+        showAllButton.click();
+        exclusiveLink.shouldBe(Condition.exactText("Эксклюзив"));
+    }
+
+    @Test
+    public void correctLogin() {
+        startup();
+
+        final String login = "тестер";
+        final String email = "cnam.sux@gmail.com";
+        final String pass = "tester228";
+
+        SelenideElement loginLink = $(By.xpath("html/body/div[3]/div[1]/header/div/div[2]/div/p/a[1]"));
+        SelenideElement loginInput= $(By.xpath(".//*[@id='login']"));
+        SelenideElement passInput = $(By.xpath(".//*[@id='password']"));
+        SelenideElement nickLink = $(By.xpath("html/body/div[2]/div[1]/header/div/div[2]/div/p/a"));
+
+        loginLink.click();
+        loginInput.shouldBe(Condition.visible).setValue(email);
+        passInput.setValue(pass);
+        passInput.pressEnter();
+        nickLink.shouldBe(Condition.visible).shouldBe(Condition.exactText(login));
+
+    }
+
+    @Test
+    public void incorrectLogin() {
+        startup();
+
+        final String email = "cam.sux@gmail.com";
+        final String pass = "tester228";
+
+        SelenideElement loginLink = $(By.xpath("html/body/div[3]/div[1]/header/div/div[2]/div/p/a[1]"));
+        SelenideElement loginInput= $(By.xpath(".//*[@id='login']"));
+        SelenideElement passInput = $(By.xpath(".//*[@id='password']"));
+        SelenideElement errorLabel = $(By.xpath(".//*[@id='LoginForm']/div[3]/div/ul/li"));
+
+        loginLink.click();
+        loginInput.shouldBe(Condition.visible).setValue(email);
+        passInput.setValue(pass);
+        passInput.pressEnter();
+        errorLabel.shouldBe(Condition.visible).shouldBe(Condition.exactText("Неверный логин или пароль"));
+    }
+
+    @Test
+    public void logout() {
+        correctLogin();
+
+        SelenideElement logoutLink = $(By.xpath("html/body/div[2]/div[1]/header/div/div[2]/div/ul/li[3]/a/span"));
+        logoutLink.shouldBe(Condition.visible).click();
+
+        SelenideElement loginLink = $(By.xpath("html/body/div[3]/div[1]/header/div/div[2]/div/p/a[1]"));
+        loginLink.shouldBe(Condition.visible).shouldBe(Condition.exactText("Войдите"));
+    }
 
 }
